383c3292cd55cf7c3f9c98cbc053dcbf0e70c2fb
TyLanger <ty.langer16@gmail.com>
2024-02-29 18:47:16 -0600

    Spawning Grids
    Spawn the 7x7 player grid and 5x5 resources grids around
    I'm not sure about the sizes. They don't look great on 16x9 moniter ratio
    
    Added some basic 48px art

--- grid.gd
+++ grid.gd
@@ -3,9 +3,25 @@ extends Node2D
 @export var width: int
 @export var height: int
 
+@export var tile_scene: PackedScene
+
+
+var grid
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	grid = []
+	for i in width:
+		var row = []
+		for j in height:
+			var tile = tile_scene.instantiate()
+			
+			var centered_x = i - width/2
+			var centered_y = j - height/2
+			tile.position = position + Vector2(48 * centered_x, 48 * centered_y)
+			add_child(tile)
+			row.append(tile)
+		grid.append(row)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
