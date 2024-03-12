1f233d79de542aa0015d284489931b0248b50d63
TyLanger <ty.langer16@gmail.com>
2024-03-11 23:43:38 -0600

    Castle Hp Bar
    Found a hp bar guide online
    It's 2 progress bars
    the health bar updates immediately
    The damage bar lags behind
    
    When the castle takes damage, start a timer. When the timer ends, the
    damage bar updates to the real hp. If you keep taking damage, the damage
    bar timer keeps resetting so the damage bar doesn't update and stays at
    what hp you had before you started taking damage
    
    Tutorial:
    https://www.youtube.com/watch?v=f90ieBOoIYQ&ab_channel=DashNothing
    
    Changed the BG colour in Project Settings > Rendering > Environment >
    clear color
    The dark purple and dark blue-green look good
    It might be a neat effect to have the bg colour change over time as the
    game gets more intense?

--- scripts/castle.gd
+++ scripts/castle.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 var health = 20
 
+@onready var health_bar = $"Castle Sprite/HealthBar"
+@onready var damage_bar = $"Castle Sprite/HealthBar/DamageBar"
+@onready var timer = $"Castle Sprite/HealthBar/Timer"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	health_bar.max_value = health
+	health_bar.value = health
+	damage_bar.max_value = health
+	damage_bar.value = health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +21,12 @@ func _process(delta):
 func take_damage():
 	health -= 1
 	if health <= 0:
+		health = 0
 		print("dead castle")
+	
+	health_bar.value = health
+	timer.start()
+
+
+func _on_timer_timeout():
+	damage_bar.value = health
